4906ec6ad0fd8c13c43585aacdb59e4c9e75dd5b
Victor Tesoura Júnior <txsoura@yahoo.com>
2024-03-19 21:03:19 -0300

    feat: add vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Main from './Main'
 
 export default async function Page() {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Main />
     </>
   )
