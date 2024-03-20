cd1c3bb1ad1193b7c3941a088370b6976230a278
Victor Tesoura Júnior <txsoura@yahoo.com>
2024-03-19 21:07:57 -0300

    style: lint code

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import Main from './Main'
 
 export default async function Page() {
