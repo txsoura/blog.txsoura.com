51c45a708492f88a3c4772d7e7b7e4beba66817b
Victor Tesoura Júnior <txsoura@yahoo.com>
2024-03-21 23:20:20 -0300

    Feat/remove vercel speed insight (#7)
    * feat: remove vercel speed insight
    
    * feat: remove speed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import Main from './Main'
 
 export default async function Page() {
   return (
     <>
       <Analytics />
-      <SpeedInsights />
       <Main />
     </>
   )
